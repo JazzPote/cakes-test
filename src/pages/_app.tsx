2ec45e828fca2d11b0ed62e18d3913cc9347ed50
Guilhem Forey <guilhem.forey@hotmail.fr>
2021-05-28 00:13:30 +0100

    perf: ⚡️ Improve accessibility and SEO

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,11 +2,21 @@ import 'antd/dist/antd.css';
 import '../sass/app.scss';
 import { AppProps } from 'next/app';
 import Layout from '../components/Layout';
+import Head from 'next/head';
 
 export default function app({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <div>
+      <Head>
+        <title>Cakes app</title>
+        <meta
+          name="description"
+          content="This ultimate cake list contains all of my favourite cakes, rated from 1 to 5. Feel free to add yours to the list."
+        />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </div>
   );
 }
