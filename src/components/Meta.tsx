d4213ef8a3d756b7b21a7d4e17fa413d1a77cdce
Guilhem Forey <guilhem.forey@hotmail.fr>
2021-05-28 08:44:23 +0100

    feat: 🎸 Made a PWA

---
+++ src/components/Meta.tsx
@@ -0,0 +1,48 @@
+const Meta = () => {
+  return (
+    <>
+      <meta name="application-name" content="Cakes App" />
+      <meta name="apple-mobile-web-app-capable" content="yes" />
+      <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+      <meta name="apple-mobile-web-app-title" content="Cakes App" />
+      <meta
+        name="description"
+        content="This ultimate cake list contains all of my favourite cakes, rated from 1 to 5. Feel free to add yours to the list."
+      />
+      <meta name="format-detection" content="telephone=no" />
+      <meta name="mobile-web-app-capable" content="yes" />
+      <meta name="msapplication-config" content="/icons/browserconfig.xml" />
+      <meta name="msapplication-TileColor" content="#2B5797" />
+      <meta name="msapplication-tap-highlight" content="no" />
+      <meta name="theme-color" content="#000000" />
+
+      <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
+      <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
+      <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
+      <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+
+      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
+      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="mask-icon" href="/icons/logo.svg" color="#5bbad5" />
+      <link rel="shortcut icon" href="/logo.svg" />
+
+      <meta name="twitter:card" content="summary" />
+      <meta name="twitter:title" content="Cakes App" />
+      <meta
+        name="twitter:description"
+        content="This ultimate cake list contains all of my favourite cakes, rated from 1 to 5. Feel free to add yours to the list."
+      />
+      <meta name="twitter:creator" content="@DavidWShadow" />
+      <meta property="og:type" content="website" />
+      <meta property="og:title" content="Cakes App" />
+      <meta
+        property="og:description"
+        content="This ultimate cake list contains all of my favourite cakes, rated from 1 to 5. Feel free to add yours to the list."
+      />
+      <meta property="og:site_name" content="Cakes App" />
+    </>
+  );
+};
+
+export default Meta;
